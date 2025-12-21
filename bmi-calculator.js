7f5b2bd331ef742dbf23285e5068911361e59b3b
Rubel <mr.adcadre41@gmail.com>
2025-12-21 22:47:17 +0600

    bmi calculator devloped

---
+++ bmi-calculator.js
@@ -0,0 +1,12 @@
+let weight = 22.9;
+let manIs;
+if (weight < 18.5) {
+  manIs = "underweight";
+} else if (weight >= 18.5 && weight <= 24.9) {
+  manIs = "Normal";
+} else if (weight >= 25 && weight <= 29.9) {
+  manIs = "Overweight";
+} else {
+  manIs = "Obese";
+}
+console.log("The person is ", manIs);
