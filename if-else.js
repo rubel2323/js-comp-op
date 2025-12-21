2110992369bbdbf948a6f8fc8740c9dbc224ada2
Rubel <mr.adcadre41@gmail.com>
2025-12-21 12:10:56 +0600

    if-else

---
+++ if-else.js
@@ -0,0 +1,8 @@
+const salary = 38000;
+const height = 65;
+const hasCar = true;
+if ((salary > 40000 && height > 65) || hasCar) {
+  console.log("Marry me,now");
+} else {
+  console.log("Go out from Here");
+}
---
+++ if.js
@@ -0,0 +1,6 @@
+let price = 500;
+if (price > 300) {
+  console.log("Do not Buy");
+} else {
+  console.log("Buy that immediatedly ");
+}
