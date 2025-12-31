beb7ddf86e22cdd554e4d48f119e9c28db965008
Rubel <mr.adcadre41@gmail.com>
2025-12-31 21:16:27 +0600

    ternary op added

---
+++ nestedIf_else.js
@@ -0,0 +1,14 @@
+const price = -300;
+if (price > 300) {
+  console.log("mama bari ja");
+} else {
+  if (price > 200) {
+    console.log("200 takaw uporew nai,fakira");
+  } else {
+    if (price > 100) {
+      console.log("100 takar upre ase");
+    } else {
+      console.log("taka nai");
+    }
+  }
+}
---
+++ ternary.js
@@ -0,0 +1,7 @@
+const age = 45;
+// if (age > 18) {
+//   console.log("you can vote");
+// } else {
+//   console.log("Ghumaw babu");
+// }
+age > 18 ? console.log("you can vote") : console.log("Ghumaw babu");
