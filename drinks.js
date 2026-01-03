908ccb87d4896bfb7b1f321886a7850a99e4c23b
Rubel <mr.adcadre41@gmail.com>
2026-01-03 11:39:09 +0600

    burger with coke

---
+++ drinks.js
@@ -0,0 +1,8 @@
+let drinks;
+const burgerPrice = 600;
+let coke = 30;
+if (burgerPrice > 500) {
+  console.log("You will get a coke for free of cost");
+} else {
+  console.log("you will pay for the coke", coke);
+}
