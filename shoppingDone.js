c1e6dafb647e4ab4c18487483587d4a61729d531
Rubel <mr.adcadre41@gmail.com>
2026-01-03 09:12:00 +0600

    childAllowance added

---
+++ shoppingDone.js
@@ -0,0 +1,9 @@
+let shoppingDone = false;
+let childAllowance = shoppingDone ? 10 : 5;
+
+// if (shoppingDone === true) {
+//   childAllowance = 10;
+// } else {
+//   childAllowance = 5;
+// }
+console.log(childAllowance);
