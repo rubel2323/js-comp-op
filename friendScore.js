7bc2e7de9edf075f34de83c7c13acbfe59b51299
Rubel <mr.adcadre41@gmail.com>
2026-01-03 18:47:03 +0600

    friendScore

---
+++ friendScore.js
@@ -0,0 +1,15 @@
+let freindScore = 9;
+let myScore = 8;
+if (myScore > 80) {
+  if (freindScore > 80) {
+    console.log("Go for a lunch");
+  } else if (freindScore >= 60) {
+    console.log("Good Luck next time");
+  } else if (freindScore >= 40) {
+    console.log("keep your friend's message unseen");
+  } else {
+    console.log("block your friend");
+  }
+} else {
+  console.log("go to home and sleep and act sad");
+}
